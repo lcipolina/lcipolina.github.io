026fd3682d843a91daca8ff139c1fb073874290c
lcipolina <lcipolina@gmail.com>
2024-12-25 18:42:08 +0000

    publishing

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ["_site/**/*.html"],  // Adjust the path to match your build directory
+    css: ["_site/**/*.css"],       // Include all generated CSS files
+    output: "_site/"               // Output directory after processing
+};
