032558427e5863f7f0787343647db6d4e6c79a1e
lcipolina <5190890+lcipolina@users.noreply.github.com>
2024-12-25 23:49:59 +0000

    Deploying to gh-pages from @ lcipolina/lcipolina.github.io@de97bc59c365d5caf1fd3ddae7306576049d5bbd 🚀

--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    content: ["_site/**/*.html"],  // Adjust the path to match your build directory
-    css: ["_site/**/*.css"],       // Include all generated CSS files
-    output: "_site/"               // Output directory after processing
-};
