e0139026326188b2436757f4845e2cddb444e73e
lcipolina <5190890+lcipolina@users.noreply.github.com>
2024-12-26 21:09:20 +0000

    Deploying to gh-pages from @ lcipolina/lcipolina.github.io@36c471198436591303b75fa345f23f5d514b05eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -367,16 +367,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-have-started-an-internship-at-google-deepmind-in-the-game-theory-and-agents-team-my-work-will-focus-in-multi-agent-coordination",
+          title: 'I have started an internship at Google DeepMind in the Game Theory and...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
