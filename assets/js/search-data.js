0532509a09a2ec08d2b206170c39758ef4581b92
lcipolina <5190890+lcipolina@users.noreply.github.com>
2024-12-29 22:39:09 +0000

    Deploying to gh-pages from @ lcipolina/lcipolina.github.io@1bfdb48b08d08c27922653d3c5ed2ecf09377cd3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Summary CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
