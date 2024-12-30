e99def3302aea25b3e7426e4c905d81d62adceb8
lcipolina <5190890+lcipolina@users.noreply.github.com>
2024-12-30 12:04:13 +0000

    Deploying to gh-pages from @ lcipolina/lcipolina.github.io@74faf9dc993024b08b28469c6327d96980e88c6a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -396,9 +396,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-machine-learning-applied-to-cultural-heritage",
+          title: 'Machine Learning applied to cultural heritage',
+          description: "Diffusion Models applied to the restoration of paintings and frescos.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{
